fa3914708bd48dd8bb95f0612fdb330a4c98d897
아키루트_ <gm3294@naver.com>
2020-09-14 22:08:48 +0900

    Create config_example.py

---
+++ lib/config_example.py
@@ -0,0 +1,4 @@
+# 모든 값을 설정한 후 _example을 파일명에서 지워주세요.
+PPBToken = "" # 핑퐁빌더 토큰 https://pingpong.us
+DBKRToken = "" # 한국 디스코드 봇 리스트 토큰 https://koreanbots.dev
+BotToken = "" # Discord 애플리케이션 봇 토큰 https://discord.com/developers/applications
