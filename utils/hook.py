d855020611ce617103d5aa8c508c3bb60f9833e2
LLOOOOTT <gm3294@naver.com>
2020-09-28 18:28:27 +0900

    Edit :: Code blocked terminal

--- utils/hook.py
+++ utils/hook.py
@@ -5,4 +5,4 @@ from lib import config
 async def send(content, name, avatar):
     async with aiohttp.ClientSession() as session:
         webhook = Webhook.from_url(config.WebHookURL, adapter=AsyncWebhookAdapter(session))
-        await webhook.send(content, username=name, avatar_url=avatar)
+        await webhook.send(f"```{content}```", username=name, avatar_url=avatar)
