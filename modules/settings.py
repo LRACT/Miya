46900afc6f8b205af6ad0aa60c4fe7bbc8881c3d
LLOOOOTT <gm3294@naver.com>
2020-09-30 01:41:31 +0900

    Hotfix :: table typo

--- modules/settings.py
+++ modules/settings.py
@@ -118,8 +118,8 @@ class settings(commands.Cog, name="설정"):
             if result == "SUCCESS":
                 await ctx.message.add_reaction("<:cs_yes:659355468715786262>")
             else:
-                await hook.send(f"Filtering set failed. {table} Result :: {result}", "미야 Terminal", self.miya.user.avatar_url)
-                print(f"Filtering set failed. {table} Result :: {result}")
+                await hook.send(f"Filtering set failed. guilds Result :: {result}", "미야 Terminal", self.miya.user.avatar_url)
+                print(f"Filtering set failed. guilds Result :: {result}")
                 await ctx.send(f"<:cs_no:659355468816187405> {ctx.author.mention} 설정 변경 도중에 오류가 발생했습니다.\n계속해서 이런 현상이 발생한다면 https://discord.gg/mdgaSjB 로 문의해주세요.")
     
     @commands.command(name="메시지설정")
