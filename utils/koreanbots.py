507fd836ab43313321b61f0b9b2bcfd7f0afe620
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-02 15:10:55 +0900

    feat(voted): can check voted

--- utils/koreanbots.py
+++ utils/koreanbots.py
@@ -1,15 +1,15 @@
 import aiohttp
 
+# TODO: https://koreanbots.readthedocs.io/en/latest/api.html#koreanbots.Client.getBots 사용
 async def get_rank():
     num = 0
     while True:
         num += 1
         async with aiohttp.ClientSession() as cs:
-            async with cs.get(f'https://api.koreanbots.dev/bots/get?page={num}') as r:
+            async with cs.get(f"https://api.koreanbots.dev/bots/get?page={num}") as r:
                 response = await r.json()
-                data = [x['name'] for x in response['data']]
+                data = [x["name"] for x in response["data"]]
                 if "미야" in data:
-                    index = data.index('미야')
+                    index = data.index("미야")
                     result = 9 * (num - 1) + (index + 1)
                     return result
-
