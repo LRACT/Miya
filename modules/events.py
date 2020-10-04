6032d0746bfa94406bccda1eb30b31746f91c258
LLOOOOTT <gm3294@naver.com>
2020-10-04 16:39:11 +0900

    서버에 초대 시 초대 감사 메시지 DM으로 전송

--- modules/events.py
+++ modules/events.py
@@ -113,6 +113,15 @@ class handler(commands.Cog, name="이벤트 리스너"):
     async def on_guild_join(self, guild):
         await webhook.terminal(f"Added to {guild.name} ( {guild.id} )", "미야 Terminal", self.miya.user.avatar_url)
         print(f"Added to {guild.name} ( {guild.id} )")
+        try:
+            embed = discord.Embed(title="미야를 초대해주셔서 감사해요!", 
+                description="""`미야야 채널설정 공지 < #채널 >` 명령어를 사용해 공지 채널을 설정해주세요.
+                    미야에 관련된 문의 사항은 [지원 서버](https://discord.gg/mdgaSjB)에서 하실 수 있어요!
+                    미야의 더욱 다양한 명령어는 `미야야 도움말` 명령어로 살펴보세요!
+                    """, color=0x5FE9FF)
+            await guild.owner.send(f"<:cs_notify:659355468904529920> {guild.owner.mention}", embed=embed)
+        except:
+            return
     
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
