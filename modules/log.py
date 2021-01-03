38646c78815c61e369e6f21de114182a2c605982
SRCiN <gm3294@naver.com>
2021-01-03 20:10:28 +0900

    Update log.py

--- modules/log.py
+++ modules/log.py
@@ -10,8 +10,8 @@ class log(commands.Cog, name="로그"):
         self.miya = miya
     
     async def get_channel(self, guild_id):
-        result = await data.load('guilds', 'guild', guild_id)
-        if result is not None:
+        rows = await data.fetch(f"SELECT * FROM `guilds` WHERE `guild` = '{guild_id}'")
+        if rows:
             channel = self.miya.get_channel(int(result[1]))
             if channel is not None:
                 return channel
