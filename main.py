78b3abc058fc3f6e219b562ea8273732c948149d
Shark1ight <gm3294@naver.com>
2021-02-14 18:10:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -78,43 +78,39 @@ async def on_message(msg):
         return
 
     if msg.content.startswith("미야야 "):
-        if msg.content.startswith("미야야 제한") or msg.content.startswith("미야야 jsk") or msg.content.startswith("미야야 블랙"):
-            await webhook.terminal(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
-            print(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
-            await miya.process_commands(msg)
+        rows = await data.fetch(f"SELECT * FROM `blacklist` WHERE `id` = '{msg.author.id}'")
+        mgr = await utils.get.mgr(msg.author.id)
+        fbd = await utils.get.filter(msg)
+        g = await data.fetch(f"SELECT * FROM `guilds` WHERE `guild` = '{msg.guild.id}'")
+        if rows:
+            result = rows[0]
+            await webhook.terminal(f"Command Cancelled ( Blacklisted ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
+            print(f"Command Cancelled ( Blacklisted ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
+            admin = miya.get_user(int(result[2]))
+            embed = discord.Embed(title="이런, 당신은 미야 사용이 제한되었어요!", description=f"제한에 관한 내용은 [지원 서버](https://discord.gg/mdgaSjB)로 문의해주세요.\n사유 : {result[1]}\n처리한 관리자 : {admin}\n차단된 시각 : {result[3]}", color=0xFF0000)
+            await msg.channel.send(f"<a:ban_guy:761149578216603668> {msg.author.mention} https://discord.gg/mdgaSjB", embed=embed)
+        elif fbd[0] == True:
+            date = datetime.datetime.utcnow()
+            time = await utils.get.kor_time(date)
+            await webhook.terminal(f"Command Cancelled ( Forbidden - Bad Word ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
+            print(f"Command Cancelled ( Forbidden - Bad Word ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
+            await webhook.blacklist(f"Blacklisted ( Automatic ) : {msg.author.id} - 부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}", "Blacklist Notify", miya.user.avatar_url)
+            result = await utils.data.commit(f"INSERT INTO `blacklist`(`id`, `reason`, `admin`, `datetime`) VALUES('{msg.author.id}', '부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}', '{miya.user.id}', '{time}')")
+            if result == "SUCCESS":
+                embed = discord.Embed(title="이런, 당신은 미야 사용이 제한되었어요!", description=f"제한에 관한 내용은 [지원 서버](https://discord.gg/mdgaSjB)로 문의해주세요.\n사유 : 부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}\n처리한 관리자 : {miya.user}\n차단된 시각 : {time}", color=0xFF0000)
+                await msg.channel.send(f"<a:ban_guy:761149578216603668> {msg.author.mention} https://discord.gg/mdgaSjB", embed=embed)
         elif "'" in msg.content or '"' in msg.content or "\\" in msg.content or ";" in msg.content:
             await webhook.terminal(f"Command Cancelled ( Forbidden - Symbols ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
             print(f"Command Cancelled ( Forbidden - Symbols ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
             await msg.channel.send(f"""<:cs_console:659355468786958356> {msg.author.mention} 미야의 오류 방지를 위해 특정 특수문자의 사용을 금지합니다.""")
+        elif g or msg.content == "미야야 등록":
+            await webhook.terminal(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
+            print(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
+            await miya.process_commands(msg)
         else:
-            rows = await data.fetch(f"SELECT * FROM `blacklist` WHERE `id` = '{msg.author.id}'")
-            fbd = await utils.get.filter(msg)
-            g = await data.fetch(f"SELECT * FROM `guilds` WHERE `guild` = '{msg.guild.id}'")
-            if rows:
-                result = rows[0]
-                await webhook.terminal(f"Command Cancelled ( Blacklisted ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
-                print(f"Command Cancelled ( Blacklisted ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
-                admin = miya.get_user(int(result[2]))
-                embed = discord.Embed(title="이런, 당신은 미야 사용이 제한되었어요!", description=f"제한에 관한 내용은 [지원 서버](https://discord.gg/mdgaSjB)로 문의해주세요.\n사유 : {result[1]}\n처리한 관리자 : {admin}\n차단된 시각 : {result[3]}", color=0xFF0000)
-                await msg.channel.send(f"<a:ban_guy:761149578216603668> {msg.author.mention} https://discord.gg/mdgaSjB", embed=embed)
-            elif fbd[0] == True:
-                date = datetime.datetime.utcnow()
-                time = await utils.get.kor_time(date)
-                await webhook.terminal(f"Command Cancelled ( Forbidden - Bad Word ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
-                print(f"Command Cancelled ( Forbidden - Bad Word ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
-                await webhook.blacklist(f"Blacklisted ( Automatic ) : {msg.author.id} - 부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}", "Blacklist Notify", miya.user.avatar_url)
-                result = await utils.data.commit(f"INSERT INTO `blacklist`(`id`, `reason`, `admin`, `datetime`) VALUES('{msg.author.id}', '부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}', '{miya.user.id}', '{time}')")
-                if result == "SUCCESS":
-                    embed = discord.Embed(title="이런, 당신은 미야 사용이 제한되었어요!", description=f"제한에 관한 내용은 [지원 서버](https://discord.gg/mdgaSjB)로 문의해주세요.\n사유 : 부적절한 단어 사용 **[ 미야 자동 차단 ]** - {fbd[1]}\n처리한 관리자 : {miya.user}\n차단된 시각 : {time}", color=0xFF0000)
-                    await msg.channel.send(f"<a:ban_guy:761149578216603668> {msg.author.mention} https://discord.gg/mdgaSjB", embed=embed)
-            elif g or msg.content == "미야야 등록":
-                await webhook.terminal(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
-                print(f"Processed Command : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
-                await miya.process_commands(msg)
-            else:
-                await webhook.terminal(f"Command Cancelled ( Unregistered ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
-                print(f"Command Cancelled ( Unregistered ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
-                await msg.channel.send(f"<:cs_id:659355469034422282> {msg.author.mention} 아직 미야의 이용약관에 동의하지 않으셨어요. `미야야 등록` 명령어를 사용해 등록해보세요!")
+            await webhook.terminal(f"Command Cancelled ( Unregistered ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )", "미야 Terminal", miya.user.avatar_url)
+            print(f"Command Cancelled ( Unregistered ) : {msg.author} ( {msg.author.id} ) - {msg.content} / Guild : {msg.guild.name} ( {msg.guild.id} )")
+            await msg.channel.send(f"<:cs_id:659355469034422282> {msg.author.mention} 아직 미야의 이용약관에 동의하지 않으셨어요. `미야야 등록` 명령어를 사용해 등록해보세요!")
 
 load_modules(miya)
 miya.run(config.BotToken)
