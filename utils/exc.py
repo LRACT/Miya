522503a92bd6e6720d37447efdabe54d65aede7b
Shark1ight <gm3294@naver.com>
2021-03-05 23:25:12 +0900

    Update exc.py

--- utils/exc.py
+++ utils/exc.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
 
-class Forbidden(Exception):
+class Forbidden(commands.CheckFailure):
     def __init__(self, embed, ctx):
         self.embed = embed
         super().__init__(f'<a:ban_guy:761149578216603668> {ctx.author.mention} https://discord.gg/tu4NKbEEnn')
 
-class No_management(Exception):
+class No_management(commands.CheckFailure):
     pass
