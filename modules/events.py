b4dc228ff02b1c4a4cf9f919e501595e72c1b9ad
Shark1ight <gm3294@naver.com>
2021-03-06 19:59:02 +0900

    Update events.py

--- modules/events.py
+++ modules/events.py
@@ -74,44 +74,8 @@ class Listeners(commands.Cog, name="이벤트 리스너"):
                 usage = ctx.command.help.split("\n")[0]
                 await ctx.send(f"<:cs_console:659355468786958356> {ctx.author.mention} `{usage}`(이)가 올바른 명령어에요!")
         elif isinstance(error, commands.CommandNotFound) or isinstance(error, commands.NotOwner):
-            f = await get.filter(ctx.message)
-            rows = await data.fetch(f"SELECT * FROM `blacklist` WHERE `id` = '{ctx.author.id}'")
-            if rows:
-                admin = self.miya.get_user(int(rows[0][2]))
-                embed = discord.Embed(
-                    title=f"이런, {ctx.author}님은 차단되셨어요.",
-                    description=f"""
-차단에 관해서는 지원 서버를 방문해주세요.
-사유 : {rows[0][1]}
-관리자 : {admin}
-차단 시각 : {rows[0][3]}
-                    """,
-                    timestamp=datetime.datetime.utcnow(),
-                    color=0xFF3333
-                )
-                embed.set_author(name="이용 제한", icon_url=self.miya.user.avatar_url)
-                await webhook.terminal(f"Blocked User >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", self.miya.user.avatar_url)
-                await ctx.send(f'<a:ban_guy:761149578216603668> {ctx.author.mention} https://discord.gg/tu4NKbEEnn', embed=embed)
-            elif f[0] is True:
-                admin = self.miya.user
-                time = await get.kor_time(datetime.datetime.utcnow())
-                await data.commit(f"INSERT INTO `blacklist`(`id`, `reason`, `admin`, `datetime`) VALUES('{ctx.author.id}', '봇 사용 도중 부적절한 언행 **[Auto]** - {f[1]}', '{admin.id}', '{time}')")
-                await webhook.blacklist(f"New Block >\nVictim - {ctx.author.id}\nAdmin - {admin} ({admin.id})\nReason - 봇 사용 도중 부적절한 언행 **[Auto]** - {f[1]}", "제한 기록", self.miya.user.avatar_url)
-                embed = discord.Embed(
-                    title=f"이런, {ctx.author}님은 차단되셨어요.",
-                    description=f"""
-차단에 관해서는 지원 서버를 방문해주세요.
-사유 : 봇 사용 도중 부적절한 언행 **[Auto]** - {f[1]}
-관리자 : {admin}
-차단 시각 : {time}
-                    """,
-                    timestamp=datetime.datetime.utcnow(),
-                    color=0xFF3333
-                )
-                embed.set_author(name="이용 제한", icon_url=self.miya.user.avatar_url)
-                await webhook.terminal(f"Auto Blocked >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", self.miya.user.avatar_url)
-                await ctx.send(f'<a:ban_guy:761149578216603668> {ctx.author.mention} https://discord.gg/tu4NKbEEnn', embed=embed)
-            else:
+            p = await get.check(ctx, self.miya)
+            if p is True:
                 response_msg = None
                 url = config.PPBRequest
                 headers = {
