54416a1fa20478b199305c72daff36051bb011e8
Restyled.io <commits@restyled.io>
2021-03-06 10:59:34 +0000

    Restyled by black

--- utils/get.py
+++ utils/get.py
@@ -70,8 +70,7 @@ async def check(ctx, miya):
         if word[0] in ctx.message.content:
             forbidden = True
             banned = word[0]
-    rows = await data.fetch(
-        f"SELECT * FROM `blacklist` WHERE `id` = '{ctx.author.id}'")
+    rows = await data.fetch(f"SELECT * FROM `blacklist` WHERE `id` = '{ctx.author.id}'")
     if rows:
         if manage is not True:
             reason = rows[0][1]
