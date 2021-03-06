4e88ec3c4bb8f35a3ae62da90801e183a734458b
Shark1ight <gm3294@naver.com>
2021-03-06 19:44:00 +0900

    Update get.py

--- utils/get.py
+++ utils/get.py
@@ -6,27 +6,17 @@ import datetime
 import aiomysql
 from discord.ext import commands
 from pytz import timezone, utc
+from utils import exc, data
 locale.setlocale(locale.LC_ALL, '')
 
 async def mgr(ctx):
-    o = await aiomysql.connect(
-        host=config.MySQL['host'],
-        port=config.MySQL['port'],
-        user=config.MySQL['username'],
-        password=config.MySQL['password'],
-        db=config.MySQL['database'],
-        autocommit=True
-    )
-    c = await o.cursor()
-    await c.execute("SELECT `manager` FROM `miya`")
-    rows = await c.fetchall()
+    rows = await data.fetch("SELECT `manager` FROM `miya`")
     mgrs = rows[0][0].split(" ")
     a = None
     for m in mgrs:
         if ctx.author.id == int(m):
             return True
-    return False
-    
+    return False 
 
 async def kor_time(date):
     KST = timezone('Asia/Seoul')
@@ -60,24 +50,51 @@ async def corona():
 
             return corona_info # 순서대로 확진자, 완치, 치료, 사망
 
-async def filter(msg):
-    forbidden = False
-    banned = None
-    o = await aiomysql.connect(
-        host=config.MySQL['host'],
-        port=config.MySQL['port'],
-        user=config.MySQL['username'],
-        password=config.MySQL['password'],
-        db=config.MySQL['database'],
-        autocommit=True
-    )
-    c = await o.cursor()
-    await c.execute("SELECT * FROM `forbidden`")
-    rows = await c.fetchall()
-    for row in rows:
-        if row[0] in msg.content:
+async def check(ctx, miya):
+    mgr = await mgr(ctx)
+
+    reason, admin, time, banned, forbidden = None, None, None, None, None
+    words = await data.fetch("SELECT * FROM `forbidden`")
+    for word in words:
+        if word[0] in msg.content:
             forbidden = True
-            banned = row[0]
-    
-    value = [forbidden, banned]
-    return value
+            banned = word[0]
+    rows = await data.fetch(f"SELECT * FROM `blacklist` WHERE `id` = '{ctx.author.id}'")
+    if rows:
+        if mgr is not True:
+            reason = rows[0][1]
+            admin = miya.get_user(int(rows[0][2]))
+            time = rows[0][3]
+            await webhook.terminal(f"Blocked User >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", miya.user.avatar_url)
+        else:
+            await ctx.send("당신은 차단되었지만, 관리 권한으로 명령어를 실행했습니다.")
+            await webhook.terminal(f"Manager Bypassed >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", miya.user.avatar_url)
+            return True
+    elif forbidden is True:
+        if mgr is not True:
+            reason = f"부적절한 언행 **[Auto]** - {banned}"
+            admin = miya.user
+            time = await kor_time(datetime.datetime.utcnow())
+            await data.commit(f"INSERT INTO `blacklist`(`id`, `reason`, `admin`, `datetime`) VALUES('{ctx.author.id}', '{reason}', '{admin.id}', '{time}')")
+            await webhook.blacklist(f"New Block >\nVictim - {ctx.author.id}\nAdmin - {admin} ({admin.id})\nReason - {reason}", "제한 기록", miya.user.avatar_url)
+            await webhook.terminal(f"Forbidden >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", miya.user.avatar_url)
+        else:
+            await ctx.send("당신은 차단되었지만, 관리 권한으로 명령어를 실행했습니다.")
+            await webhook.terminal(f"Manager Bypassed >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", miya.user.avatar_url)
+            return True
+    else:
+        await webhook.terminal(f"Processed >\nUser - {ctx.author} ({ctx.author.id})\nContent - {ctx.message.content}\nGuild - {ctx.guild.name} ({ctx.guild.id})", "명령어 처리 기록", miya.user.avatar_url)
+        return True
+    embed = discord.Embed(
+        title=f"이런, {ctx.author}님은 차단되셨어요.",
+        description=f"""
+차단에 관해서는 지원 서버를 방문해주세요.
+사유 : {reason}
+관리자 : {admin}
+차단 시각 : {time}
+        """,
+        timestamp=datetime.datetime.utcnow(),
+        color=0xFF3333
+    )
+    embed.set_author(name="이용 제한", icon_url=miya.user.avatar_url)   
+    raise exc.Forbidden(embed, ctx)
