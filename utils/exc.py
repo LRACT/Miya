39d8934194912ee4387f6a3642583b7817f4a3cc
SRCiN <gm3294@naver.com>
2021-03-07 21:33:32 +0900

    class name hotfix

--- utils/exc.py
+++ utils/exc.py
@@ -9,7 +9,7 @@ class Forbidden(commands.CheckFailure):
             "<a:ban_guy:761149578216603668> https://discord.gg/tu4NKbEEnn")
 
 
-class Forbidden(commands.CheckFailure):
+class NoReg(commands.CheckFailure):
     def __init__(self, ctx):
         super().__init__(
             "<:cs_id:659355469034422282> 미야와 대화하시려면, 먼저 이용 약관에 동의하셔야 해요.\n`미야야 가입` 명령어를 사용해보세요!"
