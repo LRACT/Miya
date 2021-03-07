741ec44c5fbeac56de9789e64ce7fe4b28a31b8f
Restyled.io <commits@restyled.io>
2021-03-07 12:15:18 +0000

    Restyled by yapf

--- utils/exc.py
+++ utils/exc.py
@@ -5,7 +5,8 @@ from discord.ext import commands
 class Forbidden(commands.CheckFailure):
     def __init__(self, embed, ctx):
         self.embed = embed
-        super().__init__("<a:ban_guy:761149578216603668> https://discord.gg/tu4NKbEEnn")
+        super().__init__(
+            "<a:ban_guy:761149578216603668> https://discord.gg/tu4NKbEEnn")
 
 
 class Forbidden(commands.CheckFailure):
