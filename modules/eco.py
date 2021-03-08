c61bf1db39b8c9a25b5dd52d7a0673d1b3cb7e55
Restyled.io <commits@restyled.io>
2021-03-08 15:44:22 +0000

    Restyled by autopep8

--- modules/eco.py
+++ modules/eco.py
@@ -4,6 +4,7 @@ from utils import data
 import datetime
 import typing
 
+
 class Economy(commands.Cog, name="경제"):
     def __init__(self, miya):
         self.miya = miya
@@ -16,12 +17,16 @@ class Economy(commands.Cog, name="경제"):
         if not rows:
             await ctx.reply(f"<:cs_no:659355468816187405> {user}님의 지갑 데이터가 등록되지 않았어요.")
         else:
-            embed = discord.Embed(title=f"💳 {user}님의 지갑 정보", timestamp=datetime.datetime.utcnow(), color=0x5FE9FF)
+            embed = discord.Embed(
+                title=f"💳 {user}님의 지갑 정보", timestamp=datetime.datetime.utcnow(), color=0x5FE9FF)
             embed.add_field(name="잔여 코인", value=f"{rows[0][1]}개", inline=False)
-            embed.add_field(name="곧 더 많은 기능이 찾아옵니다...", value="새로운 기능도 많이 기대해주세요!", inline=False)
-            embed.set_thumbnail(url=user.avatar_url_as(static_format="png", size=2048))
+            embed.add_field(name="곧 더 많은 기능이 찾아옵니다...",
+                            value="새로운 기능도 많이 기대해주세요!", inline=False)
+            embed.set_thumbnail(url=user.avatar_url_as(
+                static_format="png", size=2048))
             embed.set_author(name="지갑", icon_url=self.miya.user.avatar_url)
             await ctx.reply(embed=embed)
+
 
 def setup(miya):
     miya.add_cog(Economy(miya))
