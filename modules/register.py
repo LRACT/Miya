9cae3f0bdf3a10acdfd90d6b892a972ee360c5b5
Restyled.io <commits@restyled.io>
2021-03-09 11:12:04 +0000

    Restyled by isort

--- modules/register.py
+++ modules/register.py
@@ -4,8 +4,7 @@ import locale
 import discord
 from discord.ext import commands
 
-from utils import data
-from utils import webhook
+from utils import data, webhook
 
 locale.setlocale(locale.LC_ALL, "")
 
